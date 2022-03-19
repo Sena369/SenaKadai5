3379f6788dc1527837b779da4c2e1472b9483c15
Sena <senana0116@icloud.com>
2022-03-19 09:23:41 +0900

    リファクタリング後

---
+++ KadaiChallenge5/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  KadaiChallenge5
+//
+//  Created by 澤田世那 on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var leftTextField: UITextField!
+    @IBOutlet private weak var rightTextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    @IBAction private func calculateButton(_ sender: Any) {
+    
+        guard let leftValue = leftTextField.textToFloat else {
+            return alertPresent(alertMessage.leftNonNumric)
+        }
+        
+        guard let rightValue = rightTextField.textToFloat else {
+            return alertPresent(alertMessage.rightNonNumric)
+        }
+        
+        guard rightValue != 0  else {
+            return alertPresent(alertMessage.dividedBy0)
+        }
+        
+        resultLabel.text = String(leftValue / rightValue)
+    }
+    
+    private enum alertMessage {
+        static let leftNonNumric = "割られる数を入力してください"
+        static let rightNonNumric = "割る数を入力してください"
+        static let dividedBy0 = "割る数には0を入力しないでください"
+        
+    }
+    
+    private func alertPresent (_ message: String ) -> Void {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension UITextField {
+    var textToFloat : Float? {
+        text.flatMap { Float($0) }
+    }
+}
+
+
